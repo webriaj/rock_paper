4c9d7b9da6ba6ff88772d3003d8d542c2ed3a82b
Md Riaj Khan <71939620+webriaj@users.noreply.github.com>
2021-12-30 21:54:23 +0600

    alkert

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ function game() {
     document.getElementById("compInput").innerText = computerInput;
     if(userInput === computerInput){
         result.innerText = `The match is Draw`;
+        alert('The match is Draw');
     }
     else if (userInput ==="ROCK" && computerInput === "SCISSOR" || userInput === "PAPER" && computerInput ==="ROCK" || userInput ==="SCISSOR" && computerInput === "ROCK") {
         result.innerText = `Congratulations ! You win the Match`;
+        alert('Congratulations ! You win the Match');
     }else{
         result.innerText = `Opps ! You loss the game. try again`;
+        alert('Opps ! You loss the game. try again')
     }
 
 }
