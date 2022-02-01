a2714c84771da36475f0a731aa6bf20b9155cb51
Md Riaj Khan <71939620+webriaj@users.noreply.github.com>
2022-02-01 22:17:49 +0600

    f

--- app.js
+++ app.js
@@ -13,19 +13,47 @@ function game() {
         computerInput = "SCISSOR";
     }
     document.getElementById("compInput").innerText = computerInput;
-    if(userInput === computerInput){
+    if(userInput == computerInput){
         result.innerText = `The match is Draw`;
-        alert('The match is Draw');
+        //alert('The match is Draw');
     }
-    else if (userInput ==="ROCK" && computerInput === "SCISSOR" || userInput === "PAPER" && computerInput ==="ROCK" || userInput ==="SCISSOR" && computerInput === "ROCK") {
+    else if (userInput ==="ROCK" && computerInput === "SCISSOR" || userInput === "PAPER" && computerInput ==="ROCK" || userInput ==="SCISSOR" && computerInput === "PAPER") {
         result.innerText = `Congratulations ! You win the Match`;
-        alert('Congratulations ! You win the Match');
+       // alert('Congratulations ! You win the Match');
     }else{
         result.innerText = `Opps ! You loss the game. try again`;
-        alert('Opps ! You loss the game. try again')
+       // alert('Opps ! You loss the game. try again')
     }
 
 }
+function currency() {
+    let usd = document.getElementById("usd").value;
+    usd = parseFloat(usd);
+    let bdt = document.getElementById("bdt");
+    
+    result = usd*83;
+    bdt.innerText = result;
+}
+function temperature() {
+    let fheit = document.getElementById("Farenheit").value;
+    fheit = parseFloat(fheit);
+    let celius = document.getElementById("celcius");
+    resultCelcius = (fheit-32)*(5/9);
+    resultCelcius = resultCelcius.toFixed(3);
+    celius.innerText = resultCelcius;
+}
 function clr() {
     document.getElementById("userInput").value = ' ';
+    document.getElementById("usd").value = " ";
+    document.getElementById("Farenheit").value = ' ';
 }
+
+function bgChange(){
+    let code = Math.floor(Math.random()*16777251).toString(16);
+    console.log(code);
+    code = `#${code}`
+    document.body.style.backgroundColor = code;
+    document.getElementById('colorCode').innerText = `Background color code is ${code}`;
+   
+}
+
